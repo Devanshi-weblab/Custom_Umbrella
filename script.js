01d608fbee5842576d97aa09472ee89483cda0b1
Devanshi-weblab <74019720+Devanshi-weblab@users.noreply.github.com>
2023-01-29 00:01:49 +0530

    Custom Umbrella

---
+++ script.js
@@ -0,0 +1,6 @@
+/*creating function for buttons*/
+    
+    function changeImage() {
+        document.getElementById('image1').src = 'https://img.freepik.com/free-vector/panda-bear-silhouette-logo-design-template-funny-lazy-animal-logotype-concept-icon_126523-622.jpg?w=1060&t=st=1674875748~exp=1674876348~hmac=a240cb114b9692db10d02b9c0a97d825e8b0624cc38fe642a25500832568c867';
+    }
+    
